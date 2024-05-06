e1057bd5bfabb3b2e6bee1f6736254394812996b
Kevin Lee <kevin.son.lee@gmail.com>
2024-05-06 09:29:48 -0400

    figured out the entry portion

--- entry.py
+++ entry.py
@@ -1,25 +1,37 @@
 import tkinter
 
-win = tkinter.Tk()
-win.title("Fishbowl")
-win.geometry("750x250")
-
-input_var = tkinter.StringVar()
-all_inputs = []
-
 
 def submit():
     input = input_var.get()
     all_inputs.append(input)
-    print("You inputed " + input)
-
     input_var.set("")
 
+    display()
+
+
+def display():
+    inputs = 3
+    for value in all_inputs:
+        display_label = tkinter.Label(win, text="Here are your words/phrases:",
+                                      font=('calibre', 10))
+        display_entry = tkinter.Label(win, text=value, font=('calibre', 10))
+
+        display_label.grid(row=2, column=1)
+        display_entry.grid(row=inputs, column=1)
+        inputs += 1
+
+
+win = tkinter.Tk()
+win.title("Fishbowl")
+win.geometry("500x500")
+
+input_var = tkinter.StringVar()
+all_inputs = []
 
 input_label = tkinter.Label(win, text="Input words or phrases here",
                             font=('calibre', 10, 'bold'))
 input_entry = tkinter.Entry(win, textvariable=input_var,
-                            font=('calibre', 10, 'bold'))
+                            font=('calibre', 10))
 submit_button = tkinter.Button(win, text="Submit", command=submit)
 
 input_label.grid(row=0, column=0)
---
+++ start.py
@@ -0,0 +1,8 @@
+import tkinter
+# from entry import all_inputs
+
+win = tkinter.Tk()
+win.title("Fishbowl")
+win.geometry("500x500")
+
+win.mainloop()
